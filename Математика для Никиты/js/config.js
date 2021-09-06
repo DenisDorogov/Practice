1eb79b2ef6b0922777ed82d942f093da6d33c5e8
Denis Dorogov <denisdorogov@yandex.ru>
2021-09-06 14:22:40 +0500

    Added const DIFFICULTY and other.

--- Математика для Никиты/js/config.js
+++ Математика для Никиты/js/config.js
@@ -1,23 +1,17 @@
-var countMath = 40;
-var minSum = 20;
-var maxSum = 100;
-var difMax = 99;
-var difMin = 11;
-var multArray = [];
-var difArray = [];
-var questionArray = [];
-let answerArray = [];
 let countCorrects = 0;
 let mistakes = 0;
 let mark = 0;
 let rectification = false;
 let k = 0;
 var n = 0;
-var message = ['ОТЛИЧНО', 'ХОРОШО', 'ПОЙДЁТ', 'ПЛОХО', 'УЖАСНО'];
 var penalty = 0;
 let time = Date.now();
-let storage = {};
-let marks = [
+let examples;
+
+const COUNT_EXAMPLES = 40;
+const MESSAGES = ['ОТЛИЧНО', 'ХОРОШО', 'ПОЙДЁТ', 'ПЛОХО', 'УЖАСНО'];
+
+const MARKS = [
   [0,    'DarkRed',      6, '2',  'ЖУТЬ',    0  ],
   [2.5,  'DarkOrange',   5, '3',  'УЖАСНО',  0.5],
   [3.5,  'Gold',         4, '4-', 'ПЛОХО',   0.6],
@@ -25,3 +19,10 @@ let marks = [
   [4.25, '#00cbb0',      2, '5-', 'ХОРОШО',  0.8],
   [4.75, 'MediumOrchid', 1, '5',  'ОТЛИЧНО', 0.9] 
 ];
+
+const DIFFICULTY = {
+  "summ": {"hard": {"start": 20 ,"end": 100}, "easy": {"start": 2 ,"end": 20}},
+  "diff": {"hard": {"start": 20 ,"end": 100}, "easy": {"start": 2 ,"end": 20}},
+  "mult": {"hard": {"start": 6 ,"end": 10}, "easy": {"start": 2 ,"end": 5}},
+  "div": {"hard": {"start": 6 ,"end": 10}, "easy": {"start": 2 ,"end": 5}}
+};
